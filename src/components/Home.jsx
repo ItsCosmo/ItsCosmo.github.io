3fa60d8b6023b684d6c3b29318a9dedbb81c326c
Paul Kleimeyer <itscosmo@gmail.com>
2016-10-04 20:55:52 -0700

    Home title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,10 @@ import {Link} from 'react-router';
 
 class Home extends Component {
 
+  componentDidMount() {
+    document.title = "The Kleimeyer Group"
+  }
+
   render() {
     return (
       <div></div>
